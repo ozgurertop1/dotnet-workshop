af3a25d3549385664f01bba9c241a8ccb2157a96
ozgur <ozgurertop9@gmail.com>
2024-02-13 04:33:35 +0300

    API katmanı eklendi.

--- Projeler/DotnetWorkshop/DotnetWorkshop.API/obj/Debug/net6.0/DotnetWorkshop.API.AssemblyInfo.cs
+++ Projeler/DotnetWorkshop/DotnetWorkshop.API/obj/Debug/net6.0/DotnetWorkshop.API.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DotnetWorkshop.Repository")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DotnetWorkshop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5da3d630f3290b045f641b61f4033126d30bc6")]
-[assembly: System.Reflection.AssemblyProductAttribute("DotnetWorkshop.Repository")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DotnetWorkshop.Repository")]
+[assembly: System.Reflection.AssemblyProductAttribute("DotnetWorkshop.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DotnetWorkshop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
--- Projeler/DotnetWorkshop/DotnetWorkshop.Core/obj/Debug/net6.0/DotnetWorkshop.Core.AssemblyInfo.cs
+++ Projeler/DotnetWorkshop/DotnetWorkshop.Core/obj/Debug/net6.0/DotnetWorkshop.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetWorkshop.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5241ff9faae71a6628ae88233b4527890efa8510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa5da3d630f3290b045f641b61f4033126d30bc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetWorkshop.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetWorkshop.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
