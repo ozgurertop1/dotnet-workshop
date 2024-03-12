d5bd588540fbbe7289ac3d9f199f2ede4ab6e985
ozgur <ozgurertop9@gmail.com>
2024-03-12 07:41:54 +0300

    UnitOfWork implementasyonu.

---
+++ Projeler/DotnetWorkshop/DotnetWorkshop.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using DotnetWorkshop.Core.UnitofWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetWorkshop.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+             _context = context;
+        }
+
+
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Projeler/DotnetWorkshop/DotnetWorkshop.Repository/obj/Debug/net6.0/DotnetWorkshop.Repository.AssemblyInfo.cs
+++ Projeler/DotnetWorkshop/DotnetWorkshop.Repository/obj/Debug/net6.0/DotnetWorkshop.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetWorkshop.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bd79e6c4b26bddc4bcd73919f1624170ee2738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb795aea3901e313205f91cdd34d15eea4aba90")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetWorkshop.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetWorkshop.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
